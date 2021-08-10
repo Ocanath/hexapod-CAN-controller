c7c0efb10f7eeaf55862b047c42c3dd01a593954
Jesse Cornman <ocanath@gmail.com>
2021-08-10 09:46:07 -0500

    offsets, althought not terribly well tuned and strangely buggy. wrap control and basic demo

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -5,16 +5,82 @@
 #include "uart-disp-tools.h"
 
 
-#define NUM_JOINTS 2
+#define NUM_JOINTS 9
 
 joint chain[NUM_JOINTS] = {
 		{
-				.id = 22,
+				.id = 23,
 				.frame = 1,
 				.h0_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
 				.him1_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
 				.q = 0,
-				.q_offset = 0.f,
+				.q_offset = -0.1f,
+				.tau = {.v = 0.f},
+				.qd = 0.f,
+				.misc_cmd = LED_OFF
+		},
+		{
+				.id = 24,
+				.frame = 1,
+				.h0_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
+				.him1_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
+				.q = 0,
+				.q_offset = -0.258f,
+				.tau = {.v = 0.f},
+				.qd = 0.f,
+				.misc_cmd = LED_OFF
+		},
+		{
+				.id = 25,
+				.frame = 1,
+				.h0_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
+				.him1_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
+				.q = 0,
+				.q_offset = 0.190f,
+				.tau = {.v = 0.f},
+				.qd = 0.f,
+				.misc_cmd = LED_OFF
+		},
+		{
+				.id = 26,
+				.frame = 1,
+				.h0_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
+				.him1_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
+				.q = 0,
+				.q_offset = -1.111f,
+				.tau = {.v = 0.f},
+				.qd = 0.f,
+				.misc_cmd = LED_OFF
+		},
+		{
+				.id = 27,
+				.frame = 1,
+				.h0_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
+				.him1_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
+				.q = 0,
+				.q_offset = -3.004f,
+				.tau = {.v = 0.f},
+				.qd = 0.f,
+				.misc_cmd = LED_OFF
+		},
+		{
+				.id = 28,
+				.frame = 1,
+				.h0_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
+				.him1_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
+				.q = 0,
+				.q_offset = -3.341f,
+				.tau = {.v = 0.f},
+				.qd = 0.,
+				.misc_cmd = LED_OFF
+		},
+		{
+				.id = 20,
+				.frame = 2,
+				.h0_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
+				.him1_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
+				.q = 0,
+				.q_offset = 0.598f,
 				.tau = {.v = 0.f},
 				.qd = 0.f,
 				.misc_cmd = LED_OFF
@@ -25,27 +91,27 @@ joint chain[NUM_JOINTS] = {
 				.h0_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
 				.him1_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
 				.q = 0,
-				.q_offset = 0.f,
+				.q_offset = -0.308f,
+				.tau = {.v = 0.f},
+				.qd = 0.f,
+				.misc_cmd = LED_OFF
+		},
+		{
+				.id = 22,
+				.frame = 3,
+				.h0_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
+				.him1_i = {{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}},
+				.q = 0,
+				.q_offset = 1.1163f,
 				.tau = {.v = 0.f},
 				.qd = 0.f,
 				.misc_cmd = LED_OFF
 		}
 };
 
-static inline float v_pctl(float targ, float ref, float k)
+static inline float wrap(float in)
 {
-	float thr = fmod_2pi(ref + PI) - PI;
-	float vr1 = cos_fast(thr);
-	float vr2 = sin_fast(thr);
-
-	float th_targ = fmod_2pi(targ + PI) - PI;
-	float vt1 = cos_fast(th_targ);
-	float vt2 = sin_fast(th_targ);
-
-	float vd1 = vt1 - vr1;
-	float vd2 = vt2 - vr2;
-
-	return k*(vr1*vd2 - vr2*vd1);
+	return fmod_2pi(in + PI) - PI;
 }
 
 static inline float sat_v(float in, float hth, float lth)
@@ -56,6 +122,8 @@ static inline float sat_v(float in, float hth, float lth)
 		in = lth;
 	return in;
 }
+void can_network_keyboard_discovery(void);
+
 
 int main(void)
 {
@@ -80,7 +148,7 @@ int main(void)
 	CAN_comm_misc(chain);
 	HAL_Delay(100);
 	for(int i = 0; i < NUM_JOINTS; i++)
-		chain[i].misc_cmd = DIS_UART_ENC; //chain[i].misc_cmd = EN_UART_ENC;
+		chain[i].misc_cmd = EN_UART_ENC; //chain[i].misc_cmd = EN_UART_ENC;
 	for(int i = 0; i < NUM_JOINTS; i++)
 		CAN_comm_misc(&chain[i]);
 
@@ -90,13 +158,12 @@ int main(void)
 	chain[0].tau.v = 15.f;
 	chain[1].tau.v = 0;
 	CAN_comm_motor(chain, NUM_JOINTS);
+	for(int m = 0; m < NUM_JOINTS; m++)
+		chain[m].qd=chain[m].q;
 
 	rgb_play((rgb_t){0,255,0});
-
-
-
 	uint32_t disp_ts = HAL_GetTick()+15;
-	uint32_t tstart = HAL_GetTick();
+//	can_network_keyboard_discovery();
 	while(1)
 	{
 		if(HAL_GetTick()>can_tx_ts)
@@ -134,44 +201,108 @@ int main(void)
 			prev_led_idx = led_idx;
 		}
 
-		float t = ((float)(HAL_GetTick()-tstart))*.001f;
-		if(t < 3.f)
-			chain[1].tau.v = -0.2f;
-		else if (t >= 3.f && t < 3.2f)
+//		chain[0].tau.v = 0.f;
+		float t = ((float)HAL_GetTick())*.001f;
+		for(int m = 0; m < NUM_JOINTS; m++)
 		{
-			chain[1].tau.v = -0.0f;
-			chain[1].q_offset = chain[1].q;
-
+			if(chain[m].id == 21 || chain[m].id == 26 || chain[m].id == 24)
+				chain[m].qd = 0.33f*sin_fast(t*2.f);
+			else
+				chain[m].qd = 0.f;
 		}
-		else
+		for(int m = 0; m < NUM_JOINTS; m++)
 		{
-			float qd = 2.5f*TWO_PI*(.5f*sin_fast( (3.f*t-3.2f) - HALF_PI )+.5f)+.5f;
-			float tau = (qd-chain[1].q)*0.15f;
-			chain[1].tau.v = sat_v(tau, .25f, -.25f);
+			float tau = -0.33f*wrap(chain[m].qd - chain[m].q);
+			tau = sat_v(tau, 0.2f, -0.2f);
+			chain[m].tau.v = tau;
 		}
-		chain[0].tau.v = .1f;
 		CAN_comm_motor(chain, NUM_JOINTS);
 
-		if(HAL_GetTick()>disp_ts)
-		{
-			for(int i = 0; i < NUM_JOINTS; i++)
-			{
-				int pv = (int)(chain[i].q*1000.f);
-				char sc = '+';
-				if(pv < 0)
-				{
-					sc = '-';
-					pv = -pv;
-				}
-				int pv10 = pv/1000;
-
-				sprintf(gl_print_str, "q[%d]=%c%d.%d, ", i, sc,pv10,pv);
-				print_string(gl_print_str);
-			}
-			print_string("\r\n");
-
-			disp_ts = HAL_GetTick()+15;
-		}
+//		if(HAL_GetTick()>disp_ts)
+//		{
+//			for(int i = 0; i < NUM_JOINTS; i++)
+//			{
+//				int pv = (int)(chain[i].q*1000.f);
+//				char sc = '+';
+//				if(pv < 0)
+//				{
+//					sc = '-';
+//					pv = -pv;
+//				}
+//				int pv10 = pv/1000;
+//
+//				sprintf(gl_print_str, "q[%d]=%c%d.%d, ", i, sc,pv10,pv);
+//				print_string(gl_print_str);
+//			}
+//			print_string("\r\n");
+//
+//			disp_ts = HAL_GetTick()+15;
+//		}
 
 	}
+}
+
+void can_network_keyboard_discovery(void)
+{
+
+	int can_node_discovery_idx = 0;
+	int prev_discovery_idx = 0;
+	uint32_t led_ts = 0;
+	uint8_t led_state = 0;
+	while(1)
+	{
+		uint32_t tick = HAL_GetTick();
+		if(tick > led_ts)
+		{
+			led_state = (~led_state)&1;
+			if(led_state)
+			{
+				rgb_play((rgb_t){50,100,10});
+			}
+			else
+			{
+				rgb_play((rgb_t){0,0,0});
+			}
+			led_ts = tick + 250;
+		}
+
+		uint8_t rxne = (huart2.Instance->ISR & (1 << 5)) != 0;
+		if(rxne)
+		{
+			uint16_t rdr = (uint16_t)huart2.Instance->RDR;	//read RDR, thus clearing RXNE
+			char uart_cmd = (char)rdr;
+
+			if(uart_cmd == '>')
+				can_node_discovery_idx++;
+			else if(uart_cmd == '<')
+				can_node_discovery_idx--;
+			if(can_node_discovery_idx < 0)
+				can_node_discovery_idx = 0;
+			else if(can_node_discovery_idx > 128)
+				can_node_discovery_idx = 128;//arbitary limit on node address which is lower than can-limited node address upper bound.
+
+			if(can_node_discovery_idx != prev_discovery_idx)
+			{
+				chain[0].misc_cmd = LED_OFF;
+				chain[0].id = prev_discovery_idx;
+				CAN_comm_misc(&(chain[0]));
+
+
+				chain[0].misc_cmd = EN_UART_ENC;
+				chain[0].id = can_node_discovery_idx;
+				CAN_comm_misc(&(chain[0]));
+
+				chain[0].misc_cmd = LED_ON;
+				chain[0].id = can_node_discovery_idx;
+				CAN_comm_misc(&(chain[0]));
+			}
+			prev_discovery_idx = can_node_discovery_idx;
+
+			sprintf(gl_print_str, "kbin = %c, id = %d, qenc = %d\r\n", uart_cmd, can_node_discovery_idx, (int)(chain[0].q*1000.f));
+			print_string(gl_print_str);
+		}
+		CAN_comm_motor(chain, 1);
+
+	}
+
 }
