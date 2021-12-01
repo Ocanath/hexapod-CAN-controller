d3fa9d2d60105218702a05a37fe6403a4a32805a
ocanath <ocanath@gmail.com>
2021-12-01 17:11:17 -0600

    updated the c-code printer

--- Octave/print_c_code_linkframe_list.m
+++ Octave/print_c_code_linkframe_list.m
@@ -1,15 +1,25 @@
 function print_c_code_linkframe_list(links)
-    for i = 1:size(links,2)
+    num_links = size(links,2);
+    fprintf("static const mat4_32b_t links_def[%d] = {\n",num_links);
+    for i = 1:num_links
         m = links{i};
-        fprintf("static const mat4_32b_t h32_link%d = {\n",i);
+
         fprintf("    {\n");
+        fprintf("        {    //link %d\n",i);
+        
         for r = 1:3
-            fprintf("        {%d, %d, %d, %d},\n",m(r,1), m(r,2), m(r,3), m(r,4));
+            fprintf("            {%d, %d, %d, %d},\n",m(r,1), m(r,2), m(r,3), m(r,4));
         end
         r = 4;
-        fprintf("        {%d, %d, %d, %d}\n",m(r,1), m(r,2), m(r,3), m(r,4));
+        fprintf("            {%d, %d, %d, %d}\n",m(r,1), m(r,2), m(r,3), m(r,4));
         
-        fprintf("    }\n");
-        fprintf("};\n");
+        fprintf("        }\n");
+        if(i < num_links)
+            fprintf("    },\n");
+        else
+            fprintf("    }\n");
+        end
+        
     end
+    fprintf("};\n");
 end
