fbdf8c7f8433d4346eb46e944b695346bf87f89b
ocanath <ocanath@gmail.com>
2022-08-11 18:08:44 -0500

    updates to plotlines

--- Octave/plot_lines.m
+++ Octave/plot_lines.m
@@ -3,6 +3,7 @@ function [saved_data, time] = plot_lines(num_lines, type, baud, buf_width, limit
 slist = serialportlist;
 try
     s = serialport(slist(1), baud);
+    fprintf("found serial port: %s",slist(1));
 catch
     disp("No serial port found");
 end
@@ -11,15 +12,33 @@ f = figure(1);
 H = uicontrol();
 H.Position = [0,0,0,0];
 pause(.01);
+
+colorlist=[
+    [0, 0.4470, 0.7410]
+	[0.8500, 0.3250, 0.0980]
+    [0.9290, 0.6940, 0.1250]
+    [0.4940, 0.1840, 0.5560]
+    [0.4660, 0.6740, 0.1880]
+    [0.3010, 0.7450, 0.9330]
+    [0.6350, 0.0780, 0.1840]
+    [0, 0, 1]
+    [0, 0.5, 0]
+    [1, 0, 0]
+    [0, 0.75, 0.75]
+    [0.75, 0, 0.75]
+    [0.75, 0.75, 0]
+    [0.25, 0.25, 0.25]
+];
 for i = 1:num_lines
-    anim_line(i) = animatedline;
+    if(i <= length(colorlist))
+        anim_line(i) = animatedline('Color', colorlist(i,:), 'LineWidth', 1.0);
+    elseif(i > length(colorlist))
+       anim_line(i) = animatedline;
+    end
 end
 
-anim_line(1) = animatedline('Color',[.5 0 0] );
-if(num_lines >= 3)
-    anim_line(2) = animatedline('Color',[0 .5 0]);
-    anim_line(3) = animatedline('Color', [0,0,.5] );
-end
+%OPTIONAL: shows legend on plots
+legend
 
 grid on;
 tic;
@@ -31,10 +50,22 @@ ylim([ylowerlim,yupperlim]);
 xlim([0,buf_width]);
 tic;
 
+saved_data = 0;
+time = 0;
+
 xidx = 0;
 while(ishandle(H))
     
-    data = read(s,num_lines,type);
+    try
+        data = read(s, num_lines, type);
+        
+        %for aenc
+%         data8 = read(s,5,'uint8');
+%         data = typecast(uint8(data8(1:4)),'single');
+    catch
+        disp("read timeout or error");
+        pause(0.33);
+    end
     
     if(isempty(data) ~= 1 && size(data,2) == num_lines)
         set(gcf,'Renderer','OpenGL');
@@ -80,6 +111,10 @@ while(ishandle(H))
 %         timebuf(1) = toc;
     end
     
+end
+if(saved_data == 0)
+    saved_data = linebuf;
+    time = timebuf;
 end
 
 clear s;
